9c824a726ec31b7ba42a522ca90971c7b0317e4d
Aiden8641 <aiden18288@gmail.com>
2022-03-15 13:11:24 -0400

    restart

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import "./assets/style.css"
-/* import VueTilt from "vue-tilt.js";
-VueTilt.use(VueTilt); */
-
-createApp(App).use(router).mount('#app')
