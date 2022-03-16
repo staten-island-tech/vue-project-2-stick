60ab1dbdf14addc93d91b1a773440b324f5755f4
Aiden8641 <aiden18288@gmail.com>
2022-03-16 12:52:15 -0400

    asdf

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  methods: {
+    fetchData: async function () {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`
+        );
+        const data = await response.json();
+        this.pokemons = data.results;
+        console.log(this.pokemons)
+      } catch (error) {
+        console.log("oops");
+      }
+    },
+  },
+  created: function () {
+    this.fetchData()
+  },
+};
+</script>
