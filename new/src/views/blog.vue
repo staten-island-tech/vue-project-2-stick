92560ef4cffdec830e29c58f52caf447a27a7ebb
Aiden8641 <aiden18288@gmail.com>
2022-04-28 13:12:03 -0400

    mabb

--- new/src/views/blog.vue
+++ new/src/views/blog.vue
@@ -1,42 +1,47 @@
 <template>
   <div class="card-container">
-    <!-- <Card
+    <Card
       v-for="food in recipes"
       :key="food"
       :title="food.title"
-      :instruction="food.instructionsRecipe"
-      :ingredient="food.ingredientsRecipe"
-    /> -->
+      :text="food.instructionsRecipe"
+      :item="food.ingredientsRecipe"
+    />
   </div>
 </template>
 
 <script>
-/* import Card from "../components/card.vue"; */
+import Card from "../components/card.vue";
 import { onMounted, computed } from "vue";
 import { useStore } from "vuex";
 import { getDatabase, ref, onValue } from "firebase/database";
+
 export default {
+  components: { Card },
+
   setup() {
     const db = getDatabase();
     const blogREf = ref(db, "recipe/");
     const store = useStore();
     onMounted(() => {
       onValue(blogREf, (snapshot) => {
-        const data = snapshot.val();
-        const dataList = [];
+        /* store.dispatch("getRecipe", data); */
+        snapshot.forEach(function (childSnapshot) {
+          const key = childSnapshot.key;
+          console.log(key);
+          const childData = childSnapshot.exportVal();
+          console.log(childData);
+          store.dispatch("getRecipe", childData);
+        });
 
         /* for (const recipe in data) {
           
         } */
-        console.log(dataList);
-        console.log(data);
-
-        store.dispatch("getRecipe");
         /* console.log(data); */
       });
     });
 
-    return { recipes: computed(() => store.state.recipe) };
+    return { recipes: computed(() => store.state.recipe), Card };
   },
 };
 </script>
