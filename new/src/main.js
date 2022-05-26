7513259d3da08b886a7274066dc10a7cd1e9f133
Aiden8641 <aiden18288@gmail.com>
2022-05-25 21:38:25 -0400

    Revert "Merge branch 'new'"
    This reverts commit 703eee63328511aabefc58fa6af62807fab5a54d, reversing
    changes made to f101eb0201da0602f5284c758b7626cc2c922308.

--- new/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./assets/style.css";
-
-
-import store from "./store/index.js";
-createApp(App).use(router).use(store).mount("#app");
